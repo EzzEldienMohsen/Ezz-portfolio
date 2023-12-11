71dcf1023512eae4bce84938edbef1bc109845c8
Ezz dghedy <ezzmohsend@gmail.com>
2023-12-11 19:52:26 +0200

    first commit

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,7 +12,9 @@ const ProjectCard = ({ img, url, github, title, text }) => {
       />
       <div className="capitalize p-8">
         <h2 className="text-xl font-medium tracking-wide">{title}</h2>
-        <p className="mt-4 text-slate-700 loading-loose">{text}</p>
+        <p className="mt-8 text-slate-700 loading-loose bg-white h-64">
+          {text}
+        </p>
         <div className="mt-4 flex gap-x-4">
           <a href={url} target="__blank">
             <TbWorldWww className="w-12 h-12 text-slate-500 hover:text-black duration-300" />
